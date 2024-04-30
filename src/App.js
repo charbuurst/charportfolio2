45daf4112e4aba1029e327230c78628af2c47b5d
Charlie <chn018@ucsd.edu>
2024-04-29 23:26:37 -0700

    added portfolio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          charlie's portfolio welcome.
         </a>
       </header>
     </div>
